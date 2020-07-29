f50db8fb2c015ed39efcb3645c5a457b54cc942f
leandroromano <leandrolromano@gmail.com>
2020-07-29 14:59:26 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'photo-taker';
+
+  constructor(private zone: NgZone) { }
+
+  ngOnInit() {
+    this.showCam();
+  }
+
+
+  showCam() {
+    const video: any = document.querySelector("video");
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        // tslint:disable-next-line: only-arrow-functions
+        .then(function (stream) {
+          video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+          console.log("Something went wrong!");
+        });
+      video.play();
+    }
+  }
+
 }
